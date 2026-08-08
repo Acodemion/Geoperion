36a1cab0d1643987e473cdb2cc97f0a4d91df151
Academion <jakobmilan@outlook.de>
2026-08-08 23:58:43 +0200

    Update translations.ts

--- src/app/common/language/translations/translations.ts
+++ src/app/common/language/translations/translations.ts
@@ -1,9 +1,11 @@
 import { Translation } from "./en/en.translation"
 import { TranslationRU } from "./ru/ru.translation"
+import { TranslatienDE } from "./de/de.translation"
 
 export function getTranslations(): Translation[] {
   const Default = new Translation()
   const RU = new TranslationRU()
+  const DE = new TranslationDE()
 
-  return [Default, RU]
+  return [Default, RU, DE]
 }
