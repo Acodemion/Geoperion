1ca87bc01418ffdfee5e41a53bbb083343f7669d
Academion <jakobmilan@outlook.de>
2026-08-08 23:55:37 +0200

    Update language.x.ts files

--- src/app/common/language/language.interface.ts
+++ src/app/common/language/language.interface.ts
@@ -1,4 +1,4 @@
-export const Locales = ["EN", "RU"] as const
+export const Locales = ["EN", "RU", "DE"] as const
 
 // make union type from array of locales
 export type Locale = typeof Locales[number]
